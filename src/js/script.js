e221108b4e78d28d533bf9087df3d2753eb5a4da
Tulio <pedrocaiafaborges@gmail.com>
2025-02-11 13:58:01 -0300

    Delete src directory

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('randomizeButton').addEventListener('click', function() {
-    const inputText = document.getElementById('inputText').value;
-    const words = inputText.split(' ');
-    const randomWord = words[Math.floor(Math.random() * words.length)];
-    document.getElementById('result').innerText = randomWord;
-});
